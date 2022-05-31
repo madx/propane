2b99406791814eebf97ab1751200fd8930f9ac24
madx <madx+github@yapok.org>
2022-05-31 14:48:38 +0200

    vim: fix completion & copilot

--- vim/lua/config.lua
+++ vim/lua/config.lua
@@ -70,10 +70,17 @@ cmp.setup({
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
     { name = 'buffer' },
+    { name = 'luasnip' },
     { name = 'path' },
     { name = 'cmdline' },
-  })
+  }),
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  }
 })
+
 
 local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
